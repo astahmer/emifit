6ea966efef599cd461738826e34ba72f3bd4122b
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-02-21 00:43:27 +0100

    feat(CalendarButton): selected/onChange

--- src/components/CalendarButton.tsx
+++ src/components/CalendarButton.tsx
@@ -1,39 +1,39 @@
 import {
+    Button,
+    Popover,
+    PopoverBody,
+    PopoverContent,
+    PopoverTrigger,
     useDisclosure,
     useOutsideClick,
-    Box,
-    Popover,
-    PopoverTrigger,
-    Input,
-    PopoverContent,
-    PopoverBody,
-    Button,
 } from "@chakra-ui/react";
 import {
     Calendar,
-    CalendarDate,
     CalendarControls,
-    CalendarPrevButton,
-    CalendarNextButton,
-    CalendarMonths,
+    CalendarDate,
+    CalendarDays,
     CalendarMonth,
     CalendarMonthName,
+    CalendarMonths,
+    CalendarNextButton,
+    CalendarPrevButton,
     CalendarWeek,
-    CalendarDays,
 } from "@uselessdev/datepicker";
-import { isValid, format } from "date-fns";
-import { useState, useRef, ChangeEvent, useEffect } from "react";
+import { format } from "date-fns";
+import { useRef } from "react";
 
-const today = new Date();
-export const CalendarButton = () => {
-    const [date, setDate] = useState<CalendarDate>(today);
+export const CalendarButton = ({
+    selectedDate,
+    onChange,
+}: {
+    selectedDate: CalendarDate;
+    onChange: (update: CalendarDate) => void;
+}) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-
     const calendarRef = useRef(null);
 
     const handleSelectDate = (date: CalendarDate) => {
-        setDate(date);
-        //   setValue(() => (isValid(date) ? format(date, 'MM/dd/yyyy') : ''))
+        onChange(date);
         onClose();
     };
 
@@ -47,7 +47,7 @@ export const CalendarButton = () => {
         <Popover placement="auto-start" isOpen={isOpen} onClose={onClose} isLazy>
             <PopoverTrigger>
                 <Button fontSize="xl" fontWeight="bold" onClick={onOpen}>
-                    {format(date, "dd/MM/yyyy")}
+                    {format(selectedDate, "dd/MM/yyyy")}
                 </Button>
             </PopoverTrigger>
 
@@ -59,7 +59,12 @@ export const CalendarButton = () => {
                 _focus={{ boxShadow: "none" }}
                 ref={calendarRef}
             >
-                <Calendar value={{ start: date }} onSelectDate={handleSelectDate} singleDateSelection>
+                <Calendar
+                    value={{ start: selectedDate }}
+                    onSelectDate={handleSelectDate}
+                    singleDateSelection
+                    disableFutureDates
+                >
                     <PopoverBody p={0}>
                         <CalendarControls>
                             <CalendarPrevButton />
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,14 @@
 import { get } from "idb-keyval";
 import { nanoid } from "nanoid";
 import { useQuery } from "react-query";
+import { atom } from "jotai";
+import { CalendarDate } from "@uselessdev/datepicker";
 
 export const makeId = () => nanoid(10);
 export const useExercises = () => useQuery<Exercise[]>("exercises", () => get("exercises"), { initialData: [] });
+
+const today = new Date();
+export const currentDateAtom = atom<CalendarDate>(today);
 
 export interface Exercise {
     id: string;
