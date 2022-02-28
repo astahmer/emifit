2e701134d11887f4c1c79c75d741131f401f0e64
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-02-28 23:29:41 +0100

    feat: add throttle fn

--- src/functions/debounce.ts
+++ src/functions/debounce.ts
@@ -5,3 +5,26 @@ export function debounce(fn, wait = 100) {
         timeout = setTimeout(() => fn.call(this, ...args), wait);
     };
 }
+
+// https://gist.github.com/beaucharman/e46b8e4d03ef30480d7f4db5a78498ca
+export function throttle(callback: Function, wait: number, immediate = false) {
+    let timeout = null;
+    let initialCall = true;
+
+    return function () {
+        const callNow = immediate && initialCall;
+        const next = () => {
+            callback.apply(this, arguments);
+            timeout = null;
+        };
+
+        if (callNow) {
+            initialCall = false;
+            next();
+        }
+
+        if (!timeout) {
+            timeout = setTimeout(next, wait);
+        }
+    };
+}
