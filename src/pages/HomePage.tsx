1592e8e175d012a5b638e1eb2d5ada881dbe8a12
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-03-20 18:29:39 +0100

    feat(Home): DailyEntry -> With|out Today|Past Daily/ExerciseList

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import { CalendarButton } from "@/components/CalendarButton";
-import { MultiSelect } from "@/components/MultiSelect";
-import { Categories } from "@/constants";
-import { currentDateAtom } from "@/store";
+import { HFlex } from "@/components/HFlex";
+import { RadioCardButton } from "@/components/RadioCard";
+import { CategoryRadioPicker } from "@/Exercises/CategoryRadioPicker";
+import { ExerciseTag } from "@/Exercises/ExerciseTag";
+import { parseDate } from "@/functions/utils";
+import { orm } from "@/orm";
+import { Exercise } from "@/orm-types";
+import { currentDailyIdAtom, currentDateAtom, isDailyTodayAtom, useDaily, useDailyInvalidate } from "@/store";
 import { ChevronLeftIcon, ChevronRightIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import {
+    Alert,
+    AlertIcon,
     Box,
-    Button,
     Divider,
     Flex,
     Heading,
@@ -13,31 +19,27 @@ import {
     IconButton,
     ListItem,
     OrderedList,
-    Popover,
-    PopoverAnchor,
-    PopoverArrow,
-    PopoverBody,
-    PopoverCloseButton,
-    PopoverContent,
+    Stack,
     Text,
-    useDisclosure,
 } from "@chakra-ui/react";
 import { addDays, isFuture } from "date-fns";
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { Fragment } from "react";
+import { useMutation, useQuery } from "react-query";
+import { Link as ReactLink } from "react-router-dom";
 
 export const HomePage = () => {
     return (
         <>
             <Header />
-            <DayExcercises />
+            <DailyEntry />
         </>
     );
 };
 
 const Header = () => {
-    const [date, setDate] = useAtom(currentDateAtom);
-    const isNextDayInFuture = isFuture(addDays(date, 1));
+    const [currentDate, setDate] = useAtom(currentDateAtom);
+    const isNextDayInFuture = isFuture(addDays(currentDate, 1));
 
     return (
         <Flex p="4" justifyContent="space-between" alignItems="center">
@@ -47,7 +49,7 @@ const Header = () => {
                 icon={<ChevronLeftIcon fontSize="32px" />}
                 onClick={() => setDate((current) => addDays(current, -1))}
             />
-            <CalendarButton selectedDate={date} onChange={setDate} />
+            <CalendarButton selectedDate={currentDate} onChange={setDate} />
             <IconButton
                 variant="unstyled"
                 aria-label="Next day"
@@ -59,78 +61,174 @@ const Header = () => {
     );
 };
 
-const DayExcercises = () => {
+const DailyEntry = () => {
+    const query = useDaily();
+    const daily = query.data;
+
     return (
-        <Flex flexDirection="column" h="100%">
-            <DayCategory />
-            <Divider mt="4" />
-            <ExerciseList />
+        <Flex flexDirection="column" h="100%" minH={0}>
+            {daily ? <WithDaily /> : <WithoutDaily />}
         </Flex>
     );
 };
 
-const DayCategory = () => {
-    const items = Categories.map((cat) => cat);
-    const { isOpen, onOpen, onClose } = useDisclosure();
+const WithoutDaily = () => {
+    const isDailyToday = useAtomValue(isDailyTodayAtom);
 
     return (
-        <Flex justifyContent="space-between" alignItems="center" px="6">
-            <Box pos="relative">
-                <Popover isOpen={isOpen} onClose={onClose} closeOnBlur>
-                    <MultiSelect
-                        isOpen={isOpen}
-                        getValue={(item) => item.id}
-                        itemToString={(item) => item.label}
-                        items={items}
-                        onChange={(item) => {
-                            console.log(item);
-                            onClose();
-                        }}
-                        isMulti={false}
-                        renderButton={(props) => (
-                            <PopoverAnchor>
-                                <Heading {...props.getButtonProps()} as="h3" size="lg">
-                                    Category 1
-                                </Heading>
-                            </PopoverAnchor>
-                        )}
-                        // <PopoverTrigger>
-                        //     <Button variant="unstyled">
-                        //     </Button>
-                        // </PopoverTrigger>
-                        renderList={({ ListComponent, ...props }) =>
-                            isOpen ? (
-                                <PopoverContent>
-                                    <PopoverArrow />
-                                    <PopoverCloseButton />
-                                    <PopoverBody>
-                                        <ListComponent {...props} />
-                                    </PopoverBody>
-                                </PopoverContent>
-                            ) : null
-                        }
-                    />
-                </Popover>
+        <HFlex h="100%" justifyContent="center">
+            {isDailyToday ? <EmptyTodayDaily /> : <EmptyPastDay />}
+        </HFlex>
+    );
+};
+
+const EmptyTodayDaily = () => {
+    const id = useAtomValue(currentDailyIdAtom);
+    const invalidate = useDailyInvalidate();
+
+    const createDaily = useMutation(
+        (category: string) =>
+            orm.daily.create(id, { id, category, date: new Date(), exerciseList: [], completedList: [] }),
+        { onSuccess: invalidate }
+    );
+
+    return (
+        <>
+            <Box m="4">
+                <Alert status="info" rounded="full" justifyContent="center">
+                    <AlertIcon />
+                    No category picked yet !
+                </Alert>
             </Box>
-
-            <Button leftIcon={<EditIcon />} colorScheme="twitter" variant="solid" onClick={onOpen}>
-                Edit
-            </Button>
-        </Flex>
+            <Divider mb="4" />
+            <Box alignSelf="center">
+                <CategoryRadioPicker onChange={createDaily.mutate} />
+            </Box>
+        </>
     );
 };
 
-const exos = [
-    { name: "Exercise 1" },
-    { name: "Exercise 2" },
-    { name: "Exercise 3" },
-    { name: "Exercise 4" },
-    { name: "Exercise 5" },
-];
-const ExerciseList = () => {
+const EmptyPastDay = () => {
+    const [currentDate, setCurrentDate] = useAtom(currentDateAtom);
+    const keysQuery = useQuery(["daily", "keys"], () => orm.daily.keys());
+    const keys = keysQuery.data || [];
+
+    const mutation = useMutation(async () => {
+        const lastFilledDaily = keys
+            .map((k) => parseDate(k.toString()))
+            .sort()
+            .reverse()
+            .find((d) => d < currentDate);
+
+        if (lastFilledDaily) {
+            setCurrentDate(lastFilledDaily);
+        }
+    });
+
     return (
-        <Flex flexDir="column" pt="2" overflow="auto" h="100%">
-            {exos.map((exo, index) => {
+        <>
+            <Box m="4">
+                <Box m="4">
+                    <Alert status="warning" rounded="full" justifyContent="center">
+                        <AlertIcon />
+                        Nothing to see here !
+                    </Alert>
+                </Box>
+            </Box>
+            {keys.length ? (
+                <>
+                    <Divider mb="4" />
+                    <Box alignSelf="center">
+                        <RadioCardButton onClick={mutation.mutate.bind(undefined)}>
+                            Jump to the last filled daily entry
+                        </RadioCardButton>
+                    </Box>
+                </>
+            ) : null}
+        </>
+    );
+};
+
+const WithDaily = () => {
+    const isDailyToday = useAtomValue(isDailyTodayAtom);
+
+    const query = useDaily();
+    const daily = query.data;
+    const hasAtLeastOneExercise = daily.exerciseList.length > 0;
+    console.log(query.data);
+
+    const updateDaily = useMutation((category: string) => orm.daily.upsert(daily.id, { category }), {
+        onSuccess: query.invalidate,
+    });
+
+    return (
+        <>
+            <CategoryRadioPicker
+                defaultValue={daily.category}
+                onChange={updateDaily.mutate}
+                isDisabled={!isDailyToday || hasAtLeastOneExercise}
+            />
+            <Divider mt="4" />
+            <DailyExerciseList />
+        </>
+    );
+};
+
+const DailyExerciseList = () => {
+    const query = useDaily();
+    const hasAtLeastOneExercise = query.data?.exerciseList.length > 0;
+
+    return hasAtLeastOneExercise ? <ExerciseList /> : <EmptyExerciseList />;
+};
+
+const EmptyExerciseList = () => {
+    const isDailyToday = useAtomValue(isDailyTodayAtom);
+
+    return isDailyToday ? <TodayEmptyExerciseList /> : <PastEmptyExerciseList />;
+};
+
+const TodayEmptyExerciseList = () => {
+    return (
+        <HFlex h="100%" justifyContent="center">
+            <Box m="4">
+                <Alert status="info" rounded="full" justifyContent="center">
+                    <AlertIcon />
+                    No exercise added yet !
+                </Alert>
+            </Box>
+            <Divider mb="4" />
+            <Box alignSelf="center">
+                <ReactLink to="/add-exercise">
+                    <RadioCardButton as="div">Add exercise</RadioCardButton>
+                </ReactLink>
+            </Box>
+            <Box alignSelf="center">
+                {/* TODO */}
+                <RadioCardButton as="div">Add program</RadioCardButton>
+            </Box>
+        </HFlex>
+    );
+};
+
+const PastEmptyExerciseList = () => {
+    return (
+        <HFlex h="100%" justifyContent="center">
+            <Box m="4">
+                <Alert status="info" rounded="full" justifyContent="center">
+                    <AlertIcon />
+                    No exercise on that day !
+                </Alert>
+            </Box>
+        </HFlex>
+    );
+};
+
+const ExerciseList = () => {
+    const query = useDaily();
+
+    return (
+        <Flex flexDir="column" overflow="auto" h="100%" pt="2" pb="8">
+            {query.data.exerciseList.map((exo, index) => {
                 return (
                     <Fragment key={index}>
                         {index > 0 && (
@@ -142,45 +240,59 @@ const ExerciseList = () => {
                     </Fragment>
                 );
             })}
+            {/* bouton cardio done yes/no + confirmation */}
+            <Divider my="4" />
+            <Box alignSelf="center">
+                <ReactLink to="/add-exercise">
+                    <RadioCardButton as="div">Add exercise</RadioCardButton>
+                </ReactLink>
+            </Box>
         </Flex>
     );
 };
 
-interface ExoItem {
-    name: string;
-}
+function ExerciseItem({ exo }: { exo: Exercise }) {
+    const isDailyToday = useAtomValue(isDailyTodayAtom);
 
-function ExerciseItem({ exo }: { exo: ExoItem }) {
     return (
         <Flex flexDirection="column" px="8">
             <Flex w="100%" alignItems="flex-end">
                 <Heading as="h4" size="md">
                     {exo.name}
                 </Heading>
-                <HStack ml="auto" mt="2">
-                    {/* TODO link to add page avec valeurs préfill */}
-                    <IconButton
-                        rounded="full"
-                        variant="solid"
-                        size="sm"
-                        colorScheme="purple"
-                        aria-label="Edit"
-                        icon={<EditIcon />}
-                    />
-                    {/* Confirmation */}
-                    <IconButton
-                        rounded="full"
-                        variant="solid"
-                        size="sm"
-                        colorScheme="red"
-                        aria-label="Delete"
-                        icon={<DeleteIcon />}
-                    />
-                </HStack>
+                {/* TODO checkbox done yes/no */}
+                {/* TODO triple dots menu ? (edit/delete/...) */}
+                {isDailyToday && (
+                    <HStack ml="auto" mt="2">
+                        {/* TODO link to add-exo-like (/edit-exercise/:id) page avec valeurs préfill */}
+                        <IconButton
+                            rounded="full"
+                            variant="solid"
+                            size="sm"
+                            colorScheme="purple"
+                            aria-label="Edit"
+                            icon={<EditIcon />}
+                        />
+                        {/* Confirmation on click */}
+                        <IconButton
+                            rounded="full"
+                            variant="solid"
+                            size="sm"
+                            colorScheme="red"
+                            aria-label="Delete"
+                            icon={<DeleteIcon />}
+                        />
+                    </HStack>
+                )}
             </Flex>
             <Text fontWeight="normal" fontSize="sm" color="gray.500">
-                XX sets of YY reps
+                {exo.series.length} sets of {exo.series.map((set) => set.reps).join("/")} reps
             </Text>
+            <Stack direction="row" mt="2">
+                {exo.tags.map((tag) => (
+                    <ExerciseTag key={tag.id} tag={tag} />
+                ))}
+            </Stack>
             <OrderedList mt="2">
                 <ListItem>Lorem ipsum dolor sit amet</ListItem>
                 <ListItem>Consectetur adipiscing elit</ListItem>
