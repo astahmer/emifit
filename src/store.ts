eafc9f9d01021dc8151888411ca6a281d653ecd6
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-03-20 18:09:02 +0100

    feat(store): useDaily / derived daily atoms

--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,37 @@
 import { sortArrayOfObjectByPropFromArray, sortBy } from "@pastable/core";
 import { CalendarDate } from "@uselessdev/datepicker";
-import { atom } from "jotai";
-import { useQuery } from "react-query";
+import { atom, useAtomValue } from "jotai";
+import { useQuery, useQueryClient } from "react-query";
 import { groupBy } from "./functions/groupBy";
 import { createBrowserHistory } from "history";
-import { makeId } from "./functions/utils";
+import { makeId, printDate } from "./functions/utils";
 import { orm } from "./orm";
 import { Exercise, Program } from "./orm-types";
 import { get, update } from "idb-keyval";
+import { isToday } from "date-fns";
 
 export const browserHistory = createBrowserHistory({ window });
+export const debugModeAtom = atom<boolean>(false);
 
 const today = new Date();
 export const currentDateAtom = atom<CalendarDate>(today);
-export const currentCategoryAtom = atom<string>(null);
-export const debugModeAtom = atom<boolean>(false);
+export const currentDailyIdAtom = atom((get) => printDate(get(currentDateAtom)));
+export const isDailyTodayAtom = atom((get) => isToday(get(currentDateAtom)));
+
+export const useDaily = () => {
+    const id = useAtomValue(currentDailyIdAtom);
+    const query = useQuery(["daily", id], () => orm.daily.get(id));
+    const invalidate = useDailyInvalidate();
+
+    return { ...query, invalidate };
+};
+
+export const useDailyInvalidate = () => {
+    const id = useAtomValue(currentDailyIdAtom);
+    const queryClient = useQueryClient();
+
+    return () => void queryClient.invalidateQueries(["daily", id]);
+};
 
 export const useExercises = () => useQuery<Exercise[]>(orm.exercise.key, () => orm.exercise.get(), { initialData: [] });
 export const useExerciseList = () => {
