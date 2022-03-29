b36b94ed25d2a4124c581f4369f2a7b395d1220c
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-03-29 20:41:44 +0200

    feat: groupBy/In

--- src/functions/groupBy.ts
+++ src/functions/groupBy.ts
@@ -1,8 +1,34 @@
-export function groupBy<Item = any>(array: Item[], keyOrGetter: keyof Item | ((item: Item) => string | number)) {
+import { ObjectLiteral } from "@pastable/core";
+
+export function groupBy<Key extends keyof T, T extends ObjectLiteral>(
+    array: T[],
+    keyOrGetter: Key
+): Record<T[Key], T[]>;
+export function groupBy<T, KeyReturnT>(
+    array: T[],
+    keyOrGetter: (item: T) => KeyReturnT
+): KeyReturnT extends string | number ? Record<KeyReturnT, T[]> : never;
+export function groupBy<Key, T>(array: T[], keyOrGetter: Key) {
     let kv;
     return array.reduce((r, a) => {
-        kv = typeof keyOrGetter === "function" ? keyOrGetter(a) : a[keyOrGetter];
+        kv = typeof keyOrGetter === "function" ? keyOrGetter(a) : a[keyOrGetter as unknown as keyof T];
+        // @ts-ignore
         r[kv] = [...(r[kv] || []), a];
         return r;
-    }, {} as Record<string | number, Item[]>);
+    }, {});
+}
+
+export function groupIn<Key extends keyof T, T extends ObjectLiteral>(array: T[], keyOrGetter: Key): Record<T[Key], T>;
+export function groupIn<T, KeyReturnT>(
+    array: T[],
+    keyOrGetter: (item: T) => KeyReturnT
+): KeyReturnT extends string | number ? Record<KeyReturnT, T> : never;
+export function groupIn<Key, T>(array: T[], keyOrGetter: Key) {
+    let kv;
+    return array.reduce((r, a) => {
+        kv = typeof keyOrGetter === "function" ? keyOrGetter(a) : a[keyOrGetter as unknown as keyof T];
+        // @ts-ignore
+        r[kv] = a;
+        return r;
+    }, {});
 }
