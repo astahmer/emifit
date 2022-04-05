2d7152603c9c44e534c402d478f23c57868d5e62
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-04-05 14:48:13 +0200

    types: AwaitFn

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,13 @@
+import tb from "ts-toolbelt";
+
 export interface WithOnChange<Value = any> {
     onChange: (v: Value) => void;
 }
 
 export type StringOrNumber = string | number;
+
+export type AwaitFn<T extends tb.Function.Function<any, P>, P = unknown> = tb.Function.Return<T> extends Promise<
+    infer R
+>
+    ? R
+    : T;
