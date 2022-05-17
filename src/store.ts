44e96805750f75e8ef7d4561c50ed32e5ce52a77
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-05-18 01:23:31 +0200

    fix(store): add log + fix history 2 change

--- src/store.ts
+++ src/store.ts
@@ -3,17 +3,23 @@ import { isToday } from "date-fns";
 import { createBrowserHistory } from "history";
 import { atom, unstable_createStore } from "jotai";
 import { printDate } from "./functions/utils";
-import { parseDailyDateFromUrl, printDailyDate } from "./orm-utils";
+import { getDailyIdFromUrl, parseDailyDateFromUrl, printDailyDate } from "./orm-utils";
 
 export const store = unstable_createStore();
 export const browserHistory = createBrowserHistory({ window });
 
+const withLogs = false;
+const log = (...args: any[]) => withLogs && console.log(...args);
+
 let wasUpdatedFromBackButton = false;
 browserHistory.listen((update) => {
+    log("browserHistory.listen update", update);
+    console.trace();
     if (update.action === "PUSH") {
         // When navigating to the homepage, sets the location.pathname to the currentDate daily entry id
         // (from: "/[anything]" to "/daily/entry/:id")
         if (update.location.pathname === "/") {
+            log("history 1", update);
             const dailyId = printDailyDate(store.get(currentDateAtom));
             return browserHistory.replace(`/daily/entry/${dailyId}`);
         }
@@ -21,7 +27,11 @@ browserHistory.listen((update) => {
         // Updates the currentDate to the current daily entry date
         // (from: "/daily/entry/:someId" to "/daily/entry/:anotherId")
         // -> when navigating using BottomTabs.Add when today's daily has not been created yet
-        if (update.location.pathname.startsWith("/daily/entry/")) {
+        if (
+            update.location.pathname.startsWith("/daily/entry/") &&
+            update.location.pathname.endsWith(getDailyIdFromUrl(update.location.pathname))
+        ) {
+            log("history 2", update, parseDailyDateFromUrl(window.location.href));
             return store.set(currentDateAtom, parseDailyDateFromUrl(window.location.href));
         }
     }
@@ -29,6 +39,7 @@ browserHistory.listen((update) => {
     // When navigating using the browser back button, sets currentDate to the date of the previous (now current) location.pathname
     // (from: "/[anything]" to "/daily/entry/:id")
     if (update.action === "POP" && update.location.pathname.startsWith("/daily/entry/")) {
+        log("history 3", update);
         wasUpdatedFromBackButton = true;
         return store.set(currentDateAtom, parseDailyDateFromUrl(window.location.href));
     }
@@ -57,6 +68,7 @@ store.sub(currentDateAtom, () => {
     // (from: "/" to "/daily/entry/:id")
     // -> when initial navigation is on the homepage
     if (browserHistory.location.pathname === "/") {
+        log("atom 1", dailyId, browserHistory.location.pathname);
         return browserHistory.replace(dailyEntryPath);
     }
 
@@ -72,6 +84,7 @@ store.sub(currentDateAtom, () => {
         // -> when initial navigation is on "/daily/entry/:id/exercise/[anything]"
         // but we shouldn't be there since it's not today's daily entry so we can't update it anymore
         if (browserHistory.location.pathname.replace(dailyEntryPath, "").startsWith("/exercise/")) {
+            log("atom 2", dailyId, browserHistory.location.pathname);
             return browserHistory.replace(dailyEntryPath);
         }
 
@@ -81,5 +94,6 @@ store.sub(currentDateAtom, () => {
     // Updates the location.pathname to the current daily entry id
     // (from: "/daily/entry/:someId" to "/daily/entry/:anotherId")
     // -> when navigating using Header.Calendar Prev/Next buttons
+    log("atom 3", dailyId, browserHistory.location.pathname);
     browserHistory.push(dailyEntryPath);
 });
