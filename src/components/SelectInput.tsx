c02870557db58c532ed1916d8a6f58a1bc4d237e
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-06-02 02:06:16 +0200

    feat(SelectInput): error prop

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -1,16 +1,20 @@
-import { FormControl, FormLabel, Select, SelectProps, forwardRef } from "@chakra-ui/react";
+import { FormControl, FormLabel, Select, SelectProps, forwardRef, FormErrorMessage } from "@chakra-ui/react";
 
 import { TextInputProps } from "@/components/TextInput";
 import { WithChildren } from "@pastable/core";
 
 export const SelectInput = forwardRef(
-    ({ id, label, children, ...props }: WithChildren & Pick<TextInputProps, "label"> & SelectProps, ref) => {
+    (
+        { id, label, children, error, ...props }: WithChildren & Pick<TextInputProps, "label" | "error"> & SelectProps,
+        ref
+    ) => {
         return (
-            <FormControl id={id}>
+            <FormControl id={id} isInvalid={!!error}>
                 <FormLabel>{label}</FormLabel>
                 <Select {...props} ref={ref}>
                     {children}
                 </Select>
+                {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
             </FormControl>
         );
     }
