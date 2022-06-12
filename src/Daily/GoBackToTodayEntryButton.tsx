80dbb8a1fc4826e2ad3c38d2f6494d90ce859950
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-06-13 00:55:04 +0200

    chore: add types to extend special btns

--- src/Daily/GoBackToTodayEntryButton.tsx
+++ src/Daily/GoBackToTodayEntryButton.tsx
@@ -1,12 +1,13 @@
 import { RadioCardButton } from "@/components/RadioCard";
 import { currentDateAtom } from "@/store";
 import { ChevronRightIcon } from "@chakra-ui/icons";
+import { ButtonProps } from "@chakra-ui/react";
 import { useSetAtom } from "jotai";
 
-export const GoBackToTodayEntryButton = () => {
+export const GoBackToTodayEntryButton = (props: ButtonProps) => {
     const setCurrentDate = useSetAtom(currentDateAtom);
     return (
-        <RadioCardButton onClick={() => setCurrentDate(new Date())} variant="solid">
+        <RadioCardButton {...props} onClick={() => setCurrentDate(new Date())} variant="solid">
             Go back to today's entry
             <ChevronRightIcon />
         </RadioCardButton>
--- src/Daily/GoToClosestPreviousDailyEntryButton.tsx
+++ src/Daily/GoToClosestPreviousDailyEntryButton.tsx
@@ -1,17 +1,18 @@
 import { RadioCardButton } from "@/components/RadioCard";
 import { currentDateAtom } from "@/store";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { ButtonProps } from "@chakra-ui/react";
 import { useSetAtom } from "jotai";
 import { useMutation } from "react-query";
 import { useLastFilledDailyDate } from "./useLastFilledDailyDate";
 
-export const GoToClosestPreviousDailyEntryButton = () => {
+export const GoToClosestPreviousDailyEntryButton = (props: ButtonProps) => {
     const setCurrentDate = useSetAtom(currentDateAtom);
     const lastFilledDaily = useLastFilledDailyDate();
     const mutation = useMutation(() => void setCurrentDate(lastFilledDaily));
 
     return (
-        <RadioCardButton onClick={mutation.mutate.bind(undefined)}>
+        <RadioCardButton {...props} onClick={mutation.mutate.bind(undefined)}>
             <ChevronLeftIcon />
             Go to the closest previous daily entry
         </RadioCardButton>
