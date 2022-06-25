3a3162edbbc9d74b759687b0dc89ec7df2dfd724
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2022-06-26 00:11:45 +0200

    types: theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,37 +1,33 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, Theme } from "@chakra-ui/react";
 import { CalendarDefaultTheme } from "@uselessdev/datepicker";
 
-export const appTheme = extendTheme(
-    CalendarDefaultTheme,
-    { config: { initialColorMode: "light" } },
-    {
-        components: {
-            CalendarControl: {
-                parts: ["button"],
-                baseStyle: {
-                    button: {
-                        h: 6,
-                        px: 2,
-                        rounded: "none",
-                        fontSize: "sm",
-                        color: "white",
-                        bgColor: "pink.400",
-                        _hover: {
-                            bgColor: "pink.300",
-                        },
-                        _focus: {
-                            outline: "none",
-                        },
+export const appTheme = extendTheme(CalendarDefaultTheme, { config: { initialColorMode: "light" } } as Theme, {
+    components: {
+        CalendarControl: {
+            parts: ["button"],
+            baseStyle: {
+                button: {
+                    h: 6,
+                    px: 2,
+                    rounded: "none",
+                    fontSize: "sm",
+                    color: "white",
+                    bgColor: "pink.400",
+                    _hover: {
+                        bgColor: "pink.300",
                     },
-                },
-            },
-            CalendarDay: {
-                variants: {
-                    filled: {
-                        color: "pink.300",
+                    _focus: {
+                        outline: "none",
                     },
                 },
             },
         },
-    }
-);
+        CalendarDay: {
+            variants: {
+                filled: {
+                    color: "pink.300",
+                },
+            },
+        },
+    },
+});
